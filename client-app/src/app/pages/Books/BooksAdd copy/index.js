1a72af8758cff4154a8b417e16a3ec3979182f3b
Abel Lopes <abel.lopes@softdesign.com.br>
2021-01-25 06:19:19 -0300

    fix import button component

---
+++ client-app/src/app/pages/Books/BooksAdd copy/index.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import {  useHistory,  useParams } from "react-router-dom";
+import styled from "styled-components";
+import BookService from "../service/books.service";
+import bookDefaul from '../../../../assets/svg/book.svg';
+
+import { Container, 
+  ContainerBookDetail, 
+  Span, 
+  BookLine,
+  Strong,
+  BookLineColLeft,
+  BookLineColRigth
+} from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBackward
+} from "@fortawesome/free-solid-svg-icons";
+import ButtonStyled from "../../../components/button";
+
+const BooksDetails = ({props}) => {
+  const history = useHistory();
+  const {id} = useParams();
+
+  const [bookDetail, setBookDetail] = useState({});
+
+  useEffect(() => {
+    const getCategories = () =>{
+      BookService.GetBookByIdService(id)
+      .then(res =>{
+        setBookDetail(res.result)
+      })
+    }
+    getCategories()
+    
+  }, [])
+
+  return (
+    <Container>
+      <ContainerBookDetail>
+        <ButtonTable ><FontAwesomeIcon icon={faBackward}/> Voltar</ButtonTable>
+      </ContainerBookDetail>
+      <ContainerBookDetail>
+        <BookLine>
+        <BookLineColLeft>
+          <img src={bookDefaul} style={{width:"80%"}}/>
+        </BookLineColLeft>
+        <BookLineColRigth>
+          <Span><Strong>Titulo:</Strong> {bookDetail.title}</Span>
+          <Span><Strong>Ano de publicação:</Strong> {bookDetail.year}</Span>
+          <Span><Strong>Autor:</Strong> {bookDetail.author}</Span>
+          <Span><Strong>Descrição:</Strong> {bookDetail.description? bookDetail.description: 'Nunhuma descrição foi informada'}</Span>
+        </BookLineColRigth>
+        </BookLine>
+      </ContainerBookDetail>
+    </Container>
+  );
+}
+const ImageBook = styled.div`
+  background-image: url(${bookDefaul});
+  
+`;
+
+const ButtonTable = styled(ButtonStyled)`
+  box-sizing: border-box;
+  display: flex;
+  height: 0px;
+  padding: 0px;
+  margin: 25px;
+  display: contents;
+`;
+
+export default BooksDetails;
